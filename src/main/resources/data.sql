8bd6fdecb51dc884658f5449317c496efd37c778
nickborges <nickborgges@gmail.com>
2020-09-18 09:32:38 -0300

    adicionando config h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO USER(name, age) VALUES('Nick Kras Borges', 99);
+INSERT INTO USER(name, age) VALUES('Fulano 1', 29);
+INSERT INTO USER(name, age) VALUES('Fulano 2', 30);
+INSERT INTO USER(name, age) VALUES('Fulano 3', 31);
+INSERT INTO USER(name, age) VALUES('Fulano 4', 32);
+INSERT INTO USER(name, age) VALUES('Fulano 5', 33);
+INSERT INTO USER(name, age) VALUES('Fulano 6', 34);
+INSERT INTO USER(name, age) VALUES('Fulano 7', 35);
+INSERT INTO USER(name, age) VALUES('Fulano 8', 36);
+INSERT INTO USER(name, age) VALUES('Fulano 9', 37);
+INSERT INTO USER(name, age) VALUES('Fulano 10', 38);
+
+--INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
+--INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
+
+--INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
+--INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 2', 'Projeto não compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
+--INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
